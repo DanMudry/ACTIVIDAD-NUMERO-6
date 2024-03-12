da33d9c6429058ff5c3110eca8b9b76a964ce2bb
Daniel Mudry <danimudry@gmail.com>
2024-03-12 10:44:50 -0300

    Hemos pintado la primer pagina

--- actividad6/src/app/pages/usuariolist/usuariolist.component.ts
+++ actividad6/src/app/pages/usuariolist/usuariolist.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { UsuariosService } from '../../services/usuarios.service';
 import { IUsuario } from '../../interfaces/iusuario.interface';
+import { UsuariocardComponent } from '../../components/usuariocard/usuariocard.component';
+import { UsuarioviewComponent } from '../usuarioview/usuarioview.component';
 
 @Component({
   selector: 'app-usuariolist',
   standalone: true,
-  imports: [],
+  imports: [UsuarioviewComponent],
   templateUrl: './usuariolist.component.html',
   styleUrl: './usuariolist.component.css',
 })
@@ -13,6 +15,7 @@ export class UsuariolistComponent {
   usuarioService = inject(UsuariosService);
   arrUsuario: IUsuario[] = [];
   page: number = 1;
+  miUsuario!: IUsuario;
 
   async ngOnInit(): Promise<void> {
     try {
--- actividad6/src/app/pages/usuarioview/usuarioview.component.ts
+++ actividad6/src/app/pages/usuarioview/usuarioview.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUsuario } from '../../interfaces/iusuario.interface';
 
 @Component({
   selector: 'app-usuarioview',
   standalone: true,
   imports: [],
   templateUrl: './usuarioview.component.html',
-  styleUrl: './usuarioview.component.css'
+  styleUrl: './usuarioview.component.css',
 })
 export class UsuarioviewComponent {
-
+  @Input() miUsuario!: IUsuario;
 }
