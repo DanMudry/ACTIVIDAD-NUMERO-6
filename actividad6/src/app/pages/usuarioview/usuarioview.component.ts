066a8b93cd81190854c512cc2eebd49ca6024634
Daniel Mudry <danimudry@gmail.com>
2024-03-12 10:46:35 -0300

    nada importante

--- actividad6/src/app/pages/usuarioview/usuarioview.component.ts
+++ actividad6/src/app/pages/usuarioview/usuarioview.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IUsuario } from '../../interfaces/iusuario.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuarioview',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './usuarioview.component.html',
   styleUrl: './usuarioview.component.css',
 })
